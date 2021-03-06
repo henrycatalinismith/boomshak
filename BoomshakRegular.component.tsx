66c7cc3e484cbf92db281e1ae71eb0472d599b70
Henry Catalini Smith <henry@catalinismith.com>
2021-03-06 21:02:29 +0100

    tidy up recursive render code

--- BoomshakRegular.component.tsx
+++ BoomshakRegular.component.tsx
@@ -1,28 +1,53 @@
 import React from "react"
-import { boomshakRegular, BoomshakRegularOptions } from "./regular"
+import {
+  boomshakRegular,
+  BoomshakRegularOptions,
+} from "./regular"
 
 export interface BoomshakRegularProps {
-  children: string,
+  text: string,
   fontSize?: number,
 }
 
-function Render({ children }) {
-  const [name, props, grandchildren] = children
-  let c = undefined
-  if (grandchildren.length > 0) {
-    c = grandchildren.map(r => <Render>{r}</Render>)
-  }
-  return React.createElement(name, props, c)
+function Render({
+  name,
+  props,
+  children = [],
+}) {
+  return React.createElement(
+    name,
+    props,
+    children.map(
+      (child, i) => React.createElement(
+        Render,
+        {
+          key: i,
+          name: child[0],
+          props: child[1],
+        },
+        child[2],
+      )
+    )
+  )
 }
 
 export function BoomshakRegular({
-  children,
+  text,
   fontSize = 16,
 }: BoomshakRegularProps): React.ReactElement {
   const options: BoomshakRegularOptions = {
     fontSize,
   }
-  const data = boomshakRegular(children, options)
-  return <Render>{data}</Render>
+  const [
+    name,
+    props,
+    children,
+  ] = boomshakRegular(
+    text,
+    options,
+  )
+  return React.createElement(
+    Render,
+    { name, props, children },
+  )
 }
-
--- BoomshakRegular.stories.tsx
+++ BoomshakRegular.stories.tsx
@@ -2,13 +2,14 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import { BoomshakRegular } from "./BoomshakRegular.component"
 
-const stories = storiesOf("<BoomshakRegular />", module)
+const stories = storiesOf("<BoomshakRegular>", module)
 
 ;[64, 32, 16].forEach(fontSize => {
   stories.add(`boomshak [${fontSize}]`, () => (
-    <BoomshakRegular fontSize={fontSize}>
-      boomshak
-    </BoomshakRegular>
+    <BoomshakRegular
+      text="boomshak"
+      fontSize={fontSize}
+    />
   ))
 })
 
