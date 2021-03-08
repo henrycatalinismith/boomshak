1ffd30ff0605bd5cb766161fe4fa76453b1e89cd
Henry Catalini Smith <henry@catalinismith.com>
2021-03-08 20:56:24 +0100

    add stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,21 @@
+const chokidar = require("chokidar")
 const createHtmlElement = require("create-html-element")
+const sass = require("sass")
 const { boomshak, compile } = require("./boomshak")
 
+function monkeypatch(cls, fn) {
+  const orig = cls.prototype[fn.name][`_PS_original`] || cls.prototype[fn.name]
+  function wrapped() {
+    return fn.bind(this, orig).apply(this, arguments)
+  }
+  wrapped[`_PS_original`] = orig
+  cls.prototype[fn.name] = wrapped
+}
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addWatchTarget("style.scss")
+
   eleventyConfig.addFilter(
     "boomshak",
     function(text, lineHeight = 32) {
@@ -20,6 +34,34 @@ module.exports = function(eleventyConfig) {
         }
       )
       return svg
+    }
+  )
+
+  setImmediate(function() {
+    const Eleventy = require("@11ty/eleventy/src/Eleventy.js")
+    if (Eleventy.prototype) {
+      function watch(original) {
+        const watcher = chokidar.watch(["style.scss"], {
+          persistent: true,
+        })
+        const reload = () => () => {
+          this.eleventyServe.reload()
+        }
+        watcher.on("add", reload(this))
+        watcher.on("change", reload(this))
+        return original.apply(this)
+      }
+      monkeypatch(Eleventy, watch)
+    }
+  })
+
+  eleventyConfig.addShortcode(
+    "css",
+    function() {
+      const { css } = sass.renderSync({
+        file: "style.scss",
+      })
+      return css
     }
   )
 }
