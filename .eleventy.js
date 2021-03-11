9d6cde90555c6c42a69ac1bcf416cf7e60e5b2b7
Henry Catalini Smith <henry@catalinismith.com>
2021-03-11 20:13:43 +0100

    tidy up animatioon

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,6 @@ const {
   compile,
   padTypeface,
   renderGlyph,
-  renderStroke,
   Boomshak,
 } = require("./boomshak")
 
@@ -36,73 +35,72 @@ function compileBoomshak(
 }
 
 function animation() {
+  const dur = Math.pow(2, 7)
+  const gap = Math.pow(2, 9)
   const typeface = padTypeface(Boomshak)
-  const chars = "abcdefghiklmnopqrstuvwxyz".split("")
+  const chars = "9876543210AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz".split("")
   const paths = chars.map(l => {
     return renderGlyph(
       typeface[l],
-      [0,0],
+      [0,2],
     )
   })
+
+  const viewBoxFn = () => [
+    -3,
+    0,
+    8,
+    8,
+  ]
+
+  const animate = (i, j) => {
+    const props = {}
+    props.attributeName = "d"
+    props.attributeTypes = "XML"
+    props.fill = "freeze"
+    props.repeatCount = 1
+
+    props.begin = `${((j+1) * dur) + (j * gap)}ms`
+    props.dur = `${dur}ms`
+    props.from = paths[j]
+    props.to = paths[j+1]
+    props.id = `a${i}${j}`
+    props["xlink:href"] = `#p${i}`
+
+    return [
+      "animate",
+      props,
+      [],
+    ]
+  }
+
+  const animatePath = ([name, props], i) => {
+    props.id = `p${i}`
+    return [
+      name,
+      props,
+      chars
+        .slice(1)
+        .map((c, j) => animate(i, j)),
+    ]
+  }
+
+  const transform = ([name, props, children]) => {
+    children[0][2] = children[0][2].map((c, i) => {
+      return animatePath(c, i)
+    })
+    return [
+      name,
+      props,
+      children,
+    ]
+  }
 
   return compileBoomshak({
     text: chars[0],
     typeface,
-    viewBoxFn: () => [
-      -3,
-      -2,
-      8,
-      8,
-    ],
-  }, e => {
-    e[2][0][2] = e[2][0][2].map((c, i) => {
-      /*
-        <animate
-          xlink:href="#p1"
-          attributeName="d"
-          attributeType="XML"
-          from="M 100 100 A 200 400 30 1 0 600 200 a 300 100 45 0 1 -300 200"
-          to="M 300 600 A 300 400 -20 1 0 400 200 a 200 600 -50 0 1 100 400"
-          dur="10s"
-          fill="freeze"
-        />
-      */
-      const props = {
-        ...c[1],
-        id: `p${i}`,
-      }
-
-      const children = chars.slice(1).map((l, j) => {
-        return [
-          "animate",
-          {
-            "id": `a${i}${j}`,
-            "xlink:href": `#p${i}`,
-            "attributeName": "d",
-            "attributeType": "XML",
-            "from": paths[j],
-            "to": paths[j+1],
-            "begin": j
-              ? `a${i}${j-1}.end`
-              : `1s;a${i}${chars.slice(1).length - 1}.end+1s`,
-            "dur": `${Math.pow(2, 7)}ms`,
-            "repeatCount": "1",
-            "keySplines": ".42 0 1 1",
-            "calcMode": "spline",
-            "fill": "freeze",
-          },
-          [],
-        ]
-      })
-
-      return [
-        c[0],
-        props,
-        children,
-      ]
-    })
-    return e
-  })
+    viewBoxFn,
+  }, transform)
 }
 
 module.exports = function(eleventyConfig) {
@@ -163,5 +161,4 @@ module.exports = function(eleventyConfig) {
       return animation()
     }
   )
-
 }
