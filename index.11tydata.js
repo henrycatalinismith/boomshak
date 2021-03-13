fe6e5eaf8fd03f1f67bfc007869a2a5312764340
Henry Catalini Smith <henry@catalinismith.com>
2021-03-13 20:27:28 +0100

    fix build error

--- index.11tydata.js
+++ index.11tydata.js
@@ -1,23 +1,7 @@
 const fs = require("fs-extra")
-const _ = require("lodash")
-const fromMarkdown = require("mdast-util-from-markdown")
 const sass = require("sass")
-console.log(Math.random())
 
 const package = fs.readJSONSync("package.json")
-const markdown = fs.readFileSync("readme.md", "utf-8")
-
-const tree = fromMarkdown(markdown)
-
-const abstract = _.find(
-  tree.children,
-  child => child.type === "html"
-    && child.value.includes('itemprop="abstract"')
-).value
-
-const readme = {
-  abstract,
-}
 
 const { css } = sass.renderSync({
   file: "style.scss",
@@ -26,5 +10,4 @@ const { css } = sass.renderSync({
 module.exports = {
   css,
   package,
-  readme,
 }
